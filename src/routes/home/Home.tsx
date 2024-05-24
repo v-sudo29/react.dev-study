05c7b36a1878037980e651123a52a45d8c92c175
v-sudo29 <117846985+v-sudo29@users.noreply.github.com>
2024-05-24 10:21:57 -0700

    Create reusable ButtonLink component

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,4 +1,40 @@
 import Logo from "@/components/common/logo/Logo"
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, ReactNode } from "react"
+
+interface ButtonLinkProps {
+  href: string
+  className: string
+  children: ReactNode
+  type: 'primary' | 'secondary'
+  size: 'md' | 'lg'
+  label: string
+  target: React.HTMLAttributeAnchorTarget
+}
+
+const ButtonLink = ({
+  href,
+  className,
+  children,
+  type = 'primary',
+  size = 'md',
+  label,
+  target = '_self',
+  ...props
+} : ButtonLinkProps & React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  const buttonVariants = ''
+
+  return (
+    <a
+      href={href}
+      className={buttonVariants}
+      aria-label={label}
+      target={target}
+      {...props}
+    >
+      {children}
+    </a>
+  )
+}
 
 const Home = () => {
   return (
@@ -17,6 +53,16 @@ const Home = () => {
           <button className='border border-[##E5E7EB] px-6 py-3 rounded-full text-[#23272F] font-semibold hover:bg-gray-50/5'>
             API Reference
           </button>
+          <ButtonLink
+            href='/'
+            className=''
+            label='Homepage'
+            target='_self'
+            type='primary'
+            size='md'
+          >
+            Test Link
+          </ButtonLink>
         </div>
       </section>
     </div>
